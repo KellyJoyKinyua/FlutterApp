1bc7668ec61334040804c5e01708f6db75041a6e
KellyJoy Kinyua <kkinyua@cbq.com.qa>
2026-06-22 10:49:51 +0300

    Add comments on the functional test code

--- my_web_app/test/functional_tests.dart
+++ my_web_app/test/functional_tests.dart
@@ -7,11 +7,14 @@ import 'package:my_web_app/main.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+//Launchaes the app and updates the UI after tapping the button
+
   testWidgets('launches the app and updates the UI after tapping the button', (
     WidgetTester tester,
   ) async {
     await tester.pumpWidget(const MyApp());
     await tester.pumpAndSettle();
+//Verifies that the initial UI is displayed correctly
 
     expect(find.text('Flutter Web App'), findsOneWidget);
     expect(find.text('Hello Flutter Web'), findsOneWidget);
@@ -19,7 +22,7 @@ void main() {
 
     await tester.tap(find.text('Hello Flutter Web'));
     await tester.pumpAndSettle();
-
+//Verifies that the UI is updated correctly after tapping the button
     expect(find.text('Button Tapped'), findsOneWidget);
     expect(find.text('Hello Flutter Web'), findsNothing);
   });
